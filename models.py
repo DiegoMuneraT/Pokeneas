97ea67b38b03a212831553b8589a189e715b1ede
Diego <damunerat@eafit.edu.co>
2023-11-14 14:15:46 -0500

    Agregando ruta a cloud storage

--- models.py
+++ models.py
@@ -9,14 +9,14 @@ class Pokenea:
 
 # Lista de Pokeneas
 pokeneas = [
-    Pokenea(1, 'Pikachu', 0.4, 'Electricidad', '#', 'La electricidad fluye como la vida misma.'),
-    Pokenea(2, 'Bulbasaur', 0.7, 'Planta/Veneno', '#', 'En la dualidad de la naturaleza, la planta y el veneno coexisten.'),
-    Pokenea(3, 'Charizard', 1.7, 'Fuego/Volador', '#', 'El fuego que arde dentro refleja la libertad en vuelo.'),
-    Pokenea(4, 'Squirtle', 0.5, 'Agua', '#', 'El agua fluye suavemente, adaptándose a su entorno con sabiduría.'),
-    Pokenea(5, 'Jigglypuff', 0.5, 'Normal/Hada', '#', 'La música es la expresión del alma, al igual que el canto de Jigglypuff.'),
-    Pokenea(6, 'Mewtwo', 2.0, 'Psíquico', '#', 'En la mente se encuentra el poder que trasciende los límites del cuerpo.'),
-    Pokenea(7, 'Gyarados', 6.5, 'Agua/Volador', '#', 'La ira convertida en fuerza, un Gyarados surge de las aguas turbulentas.'),
-    Pokenea(8, 'Eevee', 0.3, 'Normal', '#', 'La versatilidad de Eevee refleja las múltiples facetas de la vida.'),
-    Pokenea(9, 'Machamp', 1.6, 'Lucha', '#', 'La fuerza en la disciplina del entrenamiento, un Machamp domina con honor.'),
-    Pokenea(10, 'Gengar', 1.5, 'Fantasma/Veneno', '#0', 'En las sombras, Gengar acecha, recordándonos la dualidad de la luz y la oscuridad.'),
+    Pokenea(1, 'Pikachu', 0.4, 'Electricidad', 'https://storage.googleapis.com/flask_pokeneas/1.png', 'La electricidad fluye como la vida misma.'),
+    Pokenea(2, 'Bulbasaur', 0.7, 'Planta/Veneno', 'https://storage.googleapis.com/flask_pokeneas/10.png', 'En la dualidad de la naturaleza, la planta y el veneno coexisten.'),
+    Pokenea(3, 'Charizard', 1.7, 'Fuego/Volador', 'https://storage.googleapis.com/flask_pokeneas/2.png', 'El fuego que arde dentro refleja la libertad en vuelo.'),
+    Pokenea(4, 'Squirtle', 0.5, 'Agua', 'https://storage.googleapis.com/flask_pokeneas/3.png', 'El agua fluye suavemente, adaptándose a su entorno con sabiduría.'),
+    Pokenea(5, 'Jigglypuff', 0.5, 'Normal/Hada', 'https://storage.googleapis.com/flask_pokeneas/4.png', 'La música es la expresión del alma, al igual que el canto de Jigglypuff.'),
+    Pokenea(6, 'Mewtwo', 2.0, 'Psíquico', 'https://storage.googleapis.com/flask_pokeneas/5.png', 'En la mente se encuentra el poder que trasciende los límites del cuerpo.'),
+    Pokenea(7, 'Gyarados', 6.5, 'Agua/Volador', 'https://storage.googleapis.com/flask_pokeneas/6.png', 'La ira convertida en fuerza, un Gyarados surge de las aguas turbulentas.'),
+    Pokenea(8, 'Eevee', 0.3, 'Normal', 'https://storage.googleapis.com/flask_pokeneas/7.png', 'La versatilidad de Eevee refleja las múltiples facetas de la vida.'),
+    Pokenea(9, 'Machamp', 1.6, 'Lucha', 'https://storage.googleapis.com/flask_pokeneas/8.png', 'La fuerza en la disciplina del entrenamiento, un Machamp domina con honor.'),
+    Pokenea(10, 'Gengar', 1.5, 'Fantasma/Veneno', 'https://storage.googleapis.com/flask_pokeneas/9.png', 'En las sombras, Gengar acecha, recordándonos la dualidad de la luz y la oscuridad.'),
 ]
